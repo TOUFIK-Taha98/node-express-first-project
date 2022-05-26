1100744f039fc0393a9e7c9255aceaa6d46e212e
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-05-26 17:02:49 +0200

    Installation express + utilisation de middleware sans express

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-const http = require('http')
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-    console.log('INCOMING REQUEST');
-    console.log(req.method, req.url);
+const app = express();
 
-    if(req.method === "POST"){
-        let body = '';
-        req.on('end', () => {
-            const userName = body.split('=')[1]
-            console.log(body);
-            res.end('<h1>'+userName+'</h1>')
-        })
-        
-        req.on('data', (chunk) => {
-            body += chunk;
-        })
-    }else{
-        res.setHeader('Content-Type', 'text/html')
-        res.end('<form method="POST"><input type="text" name="username"><button>Create User</button></form>')
+app.use((req, res, next) => {
+    let body = '';
+    req.on('end', () => {
+        const userName = body.split('=')[1];
+        if(userName){
+            req.body = {name : userName};
+        }
+        next();
+    });
+    req.on('data', chunk => {
+        body += chunk;
+    });
+})
+app.use((req, res, next) => {
+    if(req.body){
+        return res.send('<h1>'+ req.body.name);
     }
+    res.send('<form method="POST"> <input type="text" name="username" > <button> Create User </button> </form> ')
 })
 
-server.listen(5000);
+app.get('/', (req, res) => {
+
+})
+
+app.listen(5000);
