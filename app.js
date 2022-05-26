f490bcc8c031ac3f4456d7aa9f66d270ca53d645
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-05-26 16:40:59 +0200

    Envoyer un formulaire de base comme reponse

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('INCOMING REQUEST');
     console.log(req.method, req.url);
-    res.end('<h1>Success</h1>')
+
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<form><input type="text" name="username"><button>Create User</button></form>')
 })
 
 server.listen(5000);
